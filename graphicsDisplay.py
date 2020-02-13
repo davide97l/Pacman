0578aa09f70bf7ce43343c651da9bf30111b603b
Davide <davide97ls@gmail.com>
2020-02-13 21:25:33 +0800

    Added some videos

--- graphicsDisplay.py
+++ graphicsDisplay.py
@@ -40,6 +40,18 @@ GHOST_COLORS.append(formatColor(.98, .41, .07))  # Orange
 GHOST_COLORS.append(formatColor(.1, .75, .7))  # Green
 GHOST_COLORS.append(formatColor(1.0, 0.6, 0.0))  # Yellow
 GHOST_COLORS.append(formatColor(.4, 0.13, 0.91))  # Purple
+GHOST_COLORS.append(formatColor(.9, 0, 0))  # Red
+GHOST_COLORS.append(formatColor(0, .3, .9))  # Blue
+GHOST_COLORS.append(formatColor(.98, .41, .07))  # Orange
+GHOST_COLORS.append(formatColor(.1, .75, .7))  # Green
+GHOST_COLORS.append(formatColor(1.0, 0.6, 0.0))  # Yellow
+GHOST_COLORS.append(formatColor(.4, 0.13, 0.91))  # Purple
+GHOST_COLORS.append(formatColor(.9, 0, 0))  # Red
+GHOST_COLORS.append(formatColor(0, .3, .9))  # Blue
+GHOST_COLORS.append(formatColor(.98, .41, .07))  # Orange
+GHOST_COLORS.append(formatColor(.1, .75, .7))  # Green
+GHOST_COLORS.append(formatColor(1.0, 0.6, 0.0))  # Yellow
+GHOST_COLORS.append(formatColor(.4, 0.13, 0.91))  # Purple
 
 TEAM_COLORS = GHOST_COLORS[:2]
 
